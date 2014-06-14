ae18d3546aaf67d8b9a12ca58de0a63ad9774ca3
yordany2 <yordany2@cs.man.ac.uk>
2014-06-14 18:42:46 +0300

    Main initial structure

--- main.php
+++ main.php
@@ -1,3 +1,48 @@
 <?php
-  echo "<html> </html>";
+  class htmlPage 
+  {
+  	
+  private topPage() {
+   echo "<html>";
+  }
+  
+  private headOpenTag() {
+  	echo "<head>";
+  }
+  
+  private headContents() {
+  	echo "<meta ....>";
+  }
+  
+  private headCloseTag() {
+  	echo "</head>";
+  }
+  
+  private bodyOpenTag() { 
+  	echo "<body>";
+  }
+  
+  private bodyContents() 
+  {
+  	echo "  ";
+  }
+
+  private bodyCloseTag() {
+  	echo "</body>"
+  }
+  
+  private htmlCloseTag() {
+  	echo "</html>";
+  }
+  
+  public __construct() {
+  	topPage();
+  	headOpenTag();
+  	headContents();
+  	headCloseTag();
+  	bodyOpenTag();
+  	bodyContents();
+  	bodyCloseTag();
+  	htmlCloseTag();
+  }
 ?>
