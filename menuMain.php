b1dbd7a665b41c8e7ca7c1f7af375e7172ef155b
Konstantin Lyubomirov Grigorov <k.l.grigorov@gmail.com>
2014-06-16 21:42:11 +0100

    menuMain from html converted to php.

--- menuMain.php
+++ menuMain.php
@@ -1,12 +1,16 @@
-<ul id="menuMain">
-<li><a href="index.php">Home</a></li>
-<li><a href="">Bus	Excursions</a></li>
-<li><a href="">Airplane Excursions</a></li>
-<li><a href="">Holidays</a></li>
-<li><a href="">New Year 2015</a></li>
-<li><a href="">Individual Requests</a></li>
-<li><a href="">Visit Bulgaria</a></li>
-<li><a href="">About Us</a></li>
-<li><a href="">Contact Info</a></li>
-<li><a href="">Terms and Conditions</a></li>
-</ul>
+class menuMain {
+  public function __construct(){
+		echo "<ul id= "menuMain">";
+		echo '<li><a href="index.php\">Home</a></li>';
+		echo '<li><a href="">Bus	Excursions</a></li>';
+		echo '<li><a href="">Airplane Excursions</a></li>'
+		echo '<li><a href="">Holidays</a></li>';
+		echo '<li><a href="">New Year 2015</a></li>';
+		echo '<li><a href="">Individual Requests</a></li>';
+		echo '<li><a href="">Visit Bulgaria</a></li>';
+		echo '<li><a href="">About Us</a></li>';
+		echo '<li><a href="">Contact Info</a></li>';
+		echo '<li><a href="">Terms and Conditions</a></li>';
+		echo '</ul>';
+	}
+}
