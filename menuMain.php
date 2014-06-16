766159a343f74b2956eca632647374765d04faa6
Konstantin Lyubomirov Grigorov <k.l.grigorov@gmail.com>
2014-06-16 21:16:35 +0100

    Added menuMain php file containing the html.

---
+++ menuMain.php
@@ -0,0 +1,12 @@
+<ul id="menuMain">
+<li><a href="index.php">Home</a></li>
+<li><a href="">Bus	Excursions</a></li>
+<li><a href="">Airplane Excursions</a></li>
+<li><a href="">Holidays</a></li>
+<li><a href="">New Year 2015</a></li>
+<li><a href="">Individual Requests</a></li>
+<li><a href="">Visit Bulgaria</a></li>
+<li><a href="">About Us</a></li>
+<li><a href="">Contact Info</a></li>
+<li><a href="">Terms and Conditions</a></li>
+</ul>
