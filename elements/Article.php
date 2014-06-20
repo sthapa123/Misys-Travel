d55322402cbbc37dee96cafdbb3b023730aaaf6c
Konstantin Lyubomirov Grigorov <k.l.grigorov@gmail.com>
2014-06-20 01:12:08 +0100

    Article layout version2 + content_part(updated).

--- elements/Article.php
+++ elements/Article.php
@@ -9,8 +9,7 @@
 		public function __construct()
 		{
 			/*total size of the article*/
-			echo '<div id="fullOffer">'."\n";
-				
+			echo '<div id="fullOffer">'."\n";				
 				echo '<div id="offer_title">'."\n";
 				echo '</div>'."\n";
 			  
@@ -18,8 +17,7 @@
 			  echo '</div>'."\n";
 			  
 			  /*items next to image*/
-			  echo '<div id="shortOffer">'."\n";
-			 
+			  echo '<div id="shortOffer">'."\n";			 
 				  echo '<div id="duration">'."\n";
 				  echo '</div>'."\n";
 				  
@@ -32,28 +30,31 @@
 				  
 				  /*leads to request form about the particular excursion*/
 				  echo '<div id="requestButton">'."\n";
-				  echo '</div>'."\n";
-			  
+				  echo '</div>'."\n";	  
+				/*closin tag for shortOffer*/  
 			  echo '</div>'."\n";
 			  
 			  echo '<div id="image">'."\n";
 			  echo '</div>'."\n";
-			  		  
-			  /*general description*/
-			  echo '<div id="gDescription">'."\n";
-			  echo '</div>';
-			  
-			  /*day by day description*/
-			  echo '<div id="dDescription">'."\n";
-			  echo "</div>";
-			  
-			  /*accomodation hotels*/
-			  echo '<div id="hotels">'."\n";
-			  echo '</div>'."\n";
-			  
-			  echo '<div id="priceInfo">'."\n";
-			  echo '</div>'."\n";
-			  
+
+			  echo '<div id="restOfOffer">'."\n";
+			    /*general description*/
+				  echo '<div id="gDescription">'."\n";
+				  echo '</div>';
+				  
+				  /*day by day description*/
+				  echo '<div id="dDescription">'."\n";
+				  echo "</div>";
+				  
+				  /*accomodation hotels*/
+				  echo '<div id="hotels">'."\n";
+				  echo '</div>'."\n";
+				  
+				  echo '<div id="priceInfo">'."\n";
+				  echo '</div>'."\n";
+	      /*closing tag restOfOffer*/
+				echo '</div>'."\n";		
+			/*closing tag for fullOffer*/
 		  echo '</div>'."\n";
 		}	
 	}
